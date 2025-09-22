d4b145fdded85bbc66a617c46d8b7cde681c0600
Vittorio <98349901+Teygeta@users.noreply.github.com>
2025-09-22 21:52:42 +0200

    First commit

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>
+      No UI libs. Follow only the <a href="https://jordanbrennan.hashnode.dev/tac-a-new-css-methodology">TAC CSS methodology</a>
+    </p>
+  </div>
+</template>
